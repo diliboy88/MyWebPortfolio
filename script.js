51b41c16adda4ff0b88b032ac507b340581a779c
Dylan Corea <4corea.dylan@beechencliff.org.uk>
2024-11-28 18:50:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 alert("Hello Viewers!")
 function SecretGit() {
-    document.getElementById("gittyboi").innerHTML = "Developer here, why did you press it? anyways /hackclub/hs/overhere";
+    document.getElementById("gittyboi").innerHTML = "Developer here, why did you actually press it? Anyways welcome fellow hackclubbers! I am Dylan Corea, a coder, graphic designer, 3d printer enthusiast and a building PCB maker! I am trying to improve my coding skills and also my pcb skills. if you want to contact me, go to any of the links or email me at 'developrpi@gmail.com'! ";
   }
