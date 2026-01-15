04f75cd1cc072c05c680d9183446846b10c164d6
Dylan Corea <developrpi@gmail.com>
2026-01-15 15:41:09 +0000

    Update welcome message in SecretGit function
    Making it more broad!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 alert("Hello Viewers!")
 function SecretGit() {
-    document.getElementById("gittyboi").innerHTML = "Developer here, why did you actually press it? Anyways welcome fellow hackclubbers! I am Dylan Corea, a coder, graphic designer, 3d printer enthusiast and a building PCB maker! I am trying to improve my coding skills and also my pcb skills. if you want to contact me, go to any of the links or email me at 'developrpi@gmail.com'! ";
+    document.getElementById("gittyboi").innerHTML = "Developer here, why did you actually press it? Anyways welcome people! I am Dylan Corea, a coder, graphic designer, 3d printer enthusiast and a building PCB maker! I am trying to improve my coding skills and also my pcb skills. if you want to contact me, go to any of the links or email me at 'developrpi@gmail.com'! ";
   }
